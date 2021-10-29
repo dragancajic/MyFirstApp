6828821dbfb99ee442c0adb7fdbba4410382f5c3
Dragan Cajic <cajic_dragan@yahoo.com>
2021-10-29 03:04:07 +0200

    ~ use Live Templates
    > examples of live templates in Android Studio 4 for use with Kotlin

--- app/src/main/java/org/eu5/learn_pisio/myfirstapp/MainActivity.kt
+++ app/src/main/java/org/eu5/learn_pisio/myfirstapp/MainActivity.kt
@@ -2,18 +2,30 @@ package org.eu5.learn_pisio.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import org.eu5.learn_pisio.myfirstapp.utilities.StringUtility
 import com.google.gson.GsonBuilder
+
+const val TAG = "my_log_tag"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 	    
+	    // some examples of live templates in Android Studio 4 for use with Kotlin:
+	    // ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+	    // toast + TAB -> Toast.makeText(, "", Toast.LENGTH_SHORT).show()
+	    Toast.makeText(this, "a message", Toast.LENGTH_SHORT).show()
+	    
+	    // logi -> Log.i(TAG, "onCreate: ")
+	    Log.i(TAG, "onCreate: a custom message")
+	    // logd -> Log.d(TAG, "onCreate: ")
+	    
 	    Toast.makeText(this, "This is branch 2.", Toast.LENGTH_LONG).show()
 	    
-	    // § Android Studio 4.2.2
+	    // > Android Studio 4.2.2
 	    // ``````````````````````
 	    // Version Control System (VCS): [Ctrl + D] for Git Diff (do a diff)
 	    // Compare the current file to the older version of the file.
